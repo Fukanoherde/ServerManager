dc54c3982eb86aa1373b7c0e2daf34a0908450be
pxlgama <56278178+pxlgama@users.noreply.github.com>
2020-08-18 21:59:17 +0200

    Update Main.java

--- src/de/psychogamer_95/easyservermanager/Main.java
+++ src/de/psychogamer_95/easyservermanager/Main.java
@@ -1,10 +1,12 @@
 package de.psychogamer_95.easyservermanager;
 
+import de.psychogamer_95.easyservermanager.commands.CMD_GameMode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     private Main instance;
+    private CMD_GameMode gamemode;
 
     @Override
     public void onEnable() {
@@ -16,6 +18,8 @@ public class Main extends JavaPlugin {
         // MySQL \\
 
         // Commands \\
+
+        this.gamemode = new CMD_GameMode(this);
 
         // Listener \\
 
