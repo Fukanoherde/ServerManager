6a1a30b62253502b3704a4388338749399e92ffb
pxlgama <56278178+pxlgama@users.noreply.github.com>
2020-08-18 20:39:43 +0200

    Update Main.java

--- src/de/psychogamer_95/easyservermanager/Main.java
+++ src/de/psychogamer_95/easyservermanager/Main.java
@@ -4,13 +4,29 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    private Main instance;
+
     @Override
     public void onEnable() {
 
+        // Utils \\
+
+        instance = this;
+
+        // MySQL \\
+
+        // Commands \\
+
+        // Listener \\
+
+        // Message \\
     }
 
     @Override
     public void onDisable() {
 
+        // MySQL \\
+
+        // Message \\
     }
 }
