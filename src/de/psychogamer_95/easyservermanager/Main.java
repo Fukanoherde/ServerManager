addb5ba2c8d096bbd0a6167084da5e2e2ccd6176
pxlgama <56278178+pxlgama@users.noreply.github.com>
2020-08-18 20:33:01 +0200

    Create Main.java

---
+++ src/de/psychogamer_95/easyservermanager/Main.java
@@ -0,0 +1,16 @@
+package de.psychogamer_95.easyservermanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
