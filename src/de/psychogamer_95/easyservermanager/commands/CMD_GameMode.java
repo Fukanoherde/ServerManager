2e3810db6962a6eb15558f92c7af023ee4a3b5f7
pxlgama <56278178+pxlgama@users.noreply.github.com>
2020-08-18 21:16:19 +0200

    Update CMD_GameMode.java

--- src/de/psychogamer_95/easyservermanager/commands/CMD_GameMode.java
+++ src/de/psychogamer_95/easyservermanager/commands/CMD_GameMode.java
@@ -1,9 +1,11 @@
 package de.psychogamer_95.easyservermanager.commands;
 
 import de.psychogamer_95.easyservermanager.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CMD_GameMode implements CommandExecutor {
 
@@ -14,7 +16,29 @@ public class CMD_GameMode implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender cms, Command cmd, String label, String[] args) {
+
+        if (cms instanceof Player) {
+            Player p = (Player) cms;
+            if (p.hasPermission("")) {
+                if (args.length == 1) {
+                    if (cmd.getName().equalsIgnoreCase(args[1])) {
+
+                    } else {
+                        p.sendMessage("§cGive me a number!");
+                    }
+                } else {
+                    p.sendMessage("§8Use: §7/gm <0>, <1>, <2>, <3>");
+                }
+            } else {
+
+                // NoPerm \\
+
+                p.sendMessage("");
+            }
+        } else {
+            Bukkit.getConsoleSender().sendMessage("§4ERROR: §cThis command is for the real player!");
+        }
         return false;
     }
 }
