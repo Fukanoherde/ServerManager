cca5cf48979d8b5cff2c7043bea4bae1a64f1068
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-24 18:40:42 +0200

    Update Main.java

--- src/de/psychogamer_95/easyservermanager/Main.java
+++ src/de/psychogamer_95/easyservermanager/Main.java
@@ -1,12 +1,14 @@
 package de.psychogamer_95.easyservermanager;
 
 import de.psychogamer_95.easyservermanager.commands.CMD_GameMode;
+import de.psychogamer_95.easyservermanager.commands.CMD_TimeManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     private Main instance;
     private CMD_GameMode gamemode;
+    private CMD_TimeManager time;
 
     @Override
     public void onEnable() {
@@ -20,6 +22,7 @@ public class Main extends JavaPlugin {
         // Commands \\
 
         this.gamemode = new CMD_GameMode(this);
+        this.time = new CMD_TimeManager(this);
 
         // Listener \\
 
