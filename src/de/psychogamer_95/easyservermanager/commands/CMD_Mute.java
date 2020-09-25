601d811e5779804abf377a3840ce489df315c52d
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-25 18:16:55 +0200

    Update CMD_Mute.java

--- src/de/psychogamer_95/easyservermanager/commands/CMD_Mute.java
+++ src/de/psychogamer_95/easyservermanager/commands/CMD_Mute.java
@@ -1,6 +1,8 @@
 package de.psychogamer_95.easyservermanager.commands;
 
 import de.psychogamer_95.easyservermanager.Main;
+import de.psychogamer_95.easyservermanager.api.MuteAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,6 +16,31 @@ public class CMD_Mute implements CommandExecutor {
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        return true;
+        if (sender.hasPermission("")) {
+            if (args.length >=2) {
+                String playername = args[0];
+                if (!MuteAPI.isMuted(getUUID(playername))) {
+                    sender.sendMessage(plugin.Prefix + "");
+                    return true;
+                } else {
+                    String reason = "";
+                    for(int i = 1; i < args.length; i++) {
+                        reason += args[i] + " ";
+                    }
+                    MuteAPI.Mute(getUUID(playername), playername, reason, -1);
+                    sender.sendMessage(Main.getSystem().Prefix + "§7Du hast §e" + playername + " §4PERMAMENT §7vom Chat gebannt!");
+                    return true;
+                }
+            } else {
+                sender.sendMessage(plugin.Prefix + "§bUse: §8/mute <Player> <Reason>");
+                return true;
+            }
+        } else {
+            sender.sendMessage(plugin.Prefix + plugin.NoPerm);
+            return true;
+        }
+    }
+    private String getUUID(String playername) {
+        return Bukkit.getOfflinePlayer(playername).getUniqueId().toString();
     }
 }
