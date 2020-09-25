25c6a05c6d307c542f1ae1af5af700cd3152d360
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-25 22:05:16 +0200

    Update CMD_Ban.java

--- src/de/psychogamer_95/easyservermanager/commands/CMD_Ban.java
+++ src/de/psychogamer_95/easyservermanager/commands/CMD_Ban.java
@@ -2,6 +2,7 @@ package de.psychogamer_95.easyservermanager.commands;
 
 import de.psychogamer_95.easyservermanager.Main;
 import de.psychogamer_95.easyservermanager.api.BanAPI;
+import de.psychogamer_95.easyservermanager.manager.PermManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -20,11 +21,11 @@ public class CMD_Ban implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(cmd.getName().equalsIgnoreCase("ban")) {
             Player p = (Player) sender;
-            if(sender.hasPermission("easyservermanager.ban")) {
+            if (sender.hasPermission((String) PermManager.getValue("Perms.Ban")) || sender.hasPermission((String)PermManager.getValue("Perms.All"))){
                 if(args.length >= 2) {
                     String playername = args[0];
                     if(BanAPI.isBanned(getUUID(playername))) {
-                        sender.sendMessage(Main.getSystem().Prefix + "§eDieser Spieler ist bereits gebannt!");
+                        sender.sendMessage(plugin.Prefix + "§eDieser Spieler ist bereits gebannt!");
                         return true;
                     }
                     String reason = "";
@@ -32,13 +33,13 @@ public class CMD_Ban implements CommandExecutor {
                         reason += args[i] + " ";
                     }
                     BanAPI.ban(getUUID(playername), playername, reason, -1);
-                    sender.sendMessage(Main.getSystem().Prefix + "§7Du hast §e" + playername + " §4Permanent §7gebannt!");
+                    sender.sendMessage(plugin.Prefix + "§7Du hast §e" + playername + " §4Permanent §7gebannt!");
                     return true;
                 }
-                sender.sendMessage(Main.getSystem().Prefix + "§c/ban <Spieler> <Grund>");
+                sender.sendMessage(plugin.Prefix + "§c/ban <Spieler> <Grund>");
                 return true;
             } else {
-                sender.sendMessage(Main.getSystem().Prefix + Main.getSystem().NoPerm);
+                sender.sendMessage(plugin.Prefix + plugin.NoPerm);
                 return true;
             }
         }
