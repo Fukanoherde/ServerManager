b5dd96b888ce968e62a18212979920cefd692ecf
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-25 17:49:21 +0200

    Create CMD_Check.java

---
+++ src/de/psychogamer_95/easyservermanager/commands/CMD_Check.java
@@ -0,0 +1,12 @@
+package de.psychogamer_95.easyservermanager.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CMD_Check implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return true;
+    }
+}
