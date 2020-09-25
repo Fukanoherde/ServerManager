8a368548c747e4dfbc85d8d334999a97f8766b23
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-25 22:04:17 +0200

    Update TimeManager.java

--- src/de/psychogamer_95/easyservermanager/commands/CMD_TimeManager.java
+++ src/de/psychogamer_95/easyservermanager/commands/CMD_TimeManager.java
@@ -1,6 +1,7 @@
 package de.psychogamer_95.easyservermanager.commands;
 
 import de.psychogamer_95.easyservermanager.Main;
+import de.psychogamer_95.easyservermanager.manager.PermManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -22,27 +23,27 @@ public class CMD_TimeManager implements CommandExecutor {
             Player p = (Player) sender;
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("tag")) {
-                    if (p.hasPermission("easyservermanager.time.tag")) {
+                    if (p.hasPermission((String)PermManager.getValue("Perms.Time")) || p.hasPermission((String) PermManager.getValue("Perms.All"))) {
                         p.getLocation().getWorld().setTime(2000);
-                        p.sendMessage("§3Du hast die Zeit erfolgreich auf §2Tag §3gestellt");
+                        p.sendMessage(plugin.Prefix + "§3Du hast die Zeit erfolgreich auf §2Tag §3gestellt");
                         return true;
                     } else {
 
                         // NoPerm \\
 
-                        p.sendMessage("§4Keine Rechte...");
+                        p.sendMessage(plugin.Prefix + plugin.NoPerm);
                     }
                 }
                 if (args[0].equalsIgnoreCase("nacht")) {
-                    if (p.hasPermission("easyservermanager.time.nacht")) {
+                    if (p.hasPermission((String)PermManager.getValue("Perms.Time")) || p.hasPermission((String) PermManager.getValue("Perms.All"))) {
                         p.getLocation().getWorld().setTime(13000);
-                        p.sendMessage("§3Du hast die Zeit erfolgreich auf §2Nacht §3gestellt");
+                        p.sendMessage(plugin.Prefix + "§3Du hast die Zeit erfolgreich auf §2Nacht §3gestellt");
                         return true;
                     } else {
 
                         // NoPerm \\
 
-                        p.sendMessage("§4Keine Rechte...");
+                        p.sendMessage(plugin.Prefix + plugin.NoPerm);
                     }
                 }
             } else {
