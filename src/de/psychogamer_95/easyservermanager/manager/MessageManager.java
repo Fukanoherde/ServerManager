a34e4f6e459b4ec00f36375b27bbd01c0935df09
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-29 21:05:19 +0200

    Update MessageManager.java

--- src/de/psychogamer_95/easyservermanager/manager/MessageManager.java
+++ src/de/psychogamer_95/easyservermanager/manager/MessageManager.java
@@ -7,15 +7,16 @@ public class MessageManager {
     private static FileWriter fileWriter = new FileWriter(Main.getSystem().getDataFolder().getPath(), "Messages.yml");
     public static void  loadFile(){
         setValue("Plugin.Entwickler", "PsychoGamer_95");
-        setValue("Message.NoPerm", "easyservermanager.*");
-        setValue("Message.Use_Check", "easyservermanager.ban");
-        setValue("Message.Use_GameMode", "easyservermanager.tempban");
-        setValue("Message.Use_Ban", "easyservermanager.check");
-        setValue("Message.Use_Gamemode", "easyservermanager.gamemode");
-        setValue("Message.Use_Mute", "easyservermanager.gamemode.other");
-        setValue("Message.Use_Tempban", "easyservermanager.mute");
-        setValue("Message.Use_TimeManager", "easyservermanager.time");
-        setValue("Message.Use_Unban", "easyservermanager.unban");
+        setValue("Message.Use_Kick", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Check", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_GameMode", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Ban", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Gamemode", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Mute", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Tempban", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_TimeManager", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Unban", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.Use_Weather", "&bUse: &7/weather <Sonne> or <Regen>");
 
         setValue("Message.MySQL_Connect", "&7[&3MySQL&7] &eThe database succesfully connected!");
         setValue("Message.MySQL_Connect_Error", "&7[&3MySQL&7] &4ERROR: the connection has been error!");
@@ -26,7 +27,8 @@ public class MessageManager {
         setValue("Message.MySQL_CreateMuteTable", "&7[&3MySQL&7] &eMutedPlayers Table created");
         setValue("Message.MySQL_CreateMuteTable_Error", "&7[&3MySQL&7] &4the &cMutedPlayers &4Table can not created!");
 
-        setValue("Message.UseKick", "&bUse: &7/kick <Player> <Reason>");
+        setValue("Message.SetSun", "&bWeather set to Sun!");
+        setValue("Message.SetRain", "&bWeather set to Rain!");
         setValue("Message.PlayerNotOnlineKick", "&cThe specified player is not online");
         setValue("Message.YourselfKick", "&cWhy do you want to kick yourself");
         setValue("Message.Use_Ban", "easyservermanager.check");
