a4e42e0f2490bd3d57aa3f70024ea927f2ec3a01
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2020-09-29 21:04:48 +0200

    Update CMD_Weather.java

--- src/de/psychogamer_95/easyservermanager/commands/CMD_Weather.java
+++ src/de/psychogamer_95/easyservermanager/commands/CMD_Weather.java
@@ -1,9 +1,13 @@
 package de.psychogamer_95.easyservermanager.commands;
 
 import de.psychogamer_95.easyservermanager.Main;
+import de.psychogamer_95.easyservermanager.manager.MessageManager;
+import de.psychogamer_95.easyservermanager.manager.PermManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CMD_Weather implements CommandExecutor {
 
@@ -15,6 +19,37 @@ public class CMD_Weather implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (args.length == 1) {
+                if (p.hasPermission(PermManager.getValue("Perms.Weather").toString()) || p.hasPermission("Perms.All")) {
+                    if (args[0].equalsIgnoreCase("regen")) {
+                        Bukkit.getWorld(p.getLocation().getWorld().getName()).setStorm(true);
+                        Bukkit.getWorld(p.getLocation().getWorld().getName()).setThundering(true);
+                        p.sendMessage(plugin.Prefix + MessageManager.getValue("Message.SetRain").toString().replaceAll("&", "§"));
+                        return true;
+                    }
+                } else {
+                    p.sendMessage(plugin.Prefix + plugin.NoPerm);
+                }
+            } else {
+                p.sendMessage(plugin.Prefix + MessageManager.getValue("Message.Use_Weather").toString().replaceAll("&", "§"));
+            }
+            if (p.hasPermission(PermManager.getValue("Perms.Weather").toString()) || p.hasPermission("Perms.All")) {
+                if (args[0].equalsIgnoreCase("sonne")) {
+                    Bukkit.getWorld(p.getLocation().getWorld().getName()).setStorm(false);
+                    Bukkit.getWorld(p.getLocation().getWorld().getName()).setThundering(false);
+                    p.sendMessage(plugin.Prefix + MessageManager.getValue("Message.SetSun").toString().replaceAll("&", "§"));
+                    return true;
+                } else {
+                    p.sendMessage(plugin.Prefix + MessageManager.getValue("Message.Use_Weather").toString().replaceAll("&", "§"));
+                }
+            } else {
+                p.sendMessage(plugin.Prefix + plugin.NoPerm);
+            }
+        } else {
+            sender.sendMessage("");
+        }
         return false;
     }
 }
